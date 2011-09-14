4737583a6c10456e4c0a291053e65118a9bb50ed
ricardogcolombo@gmail.com <ricardogcolombo@gmail.com@4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa>
2011-09-14 11:22:45 +0000

    Macros y el invertir c
    git-svn-id: https://022011-tp-o2.googlecode.com/svn/trunk@3 4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa

--- tp2-entregable/src/invertir_asm.asm
+++ tp2-entregable/src/invertir_asm.asm
@@ -1,9 +1,34 @@
 ; void invertir_asm (unsigned char *src, unsigned char *dst, int h, int w, int row_size)
 
+
 global invertir_asm
 
+%include "macros.asm"
+
+%define src [ebp+8]
+%define dst [ebp+12]
+%define alto [ebp+16]
+%define ancho [ebp+20]
+%define RZ [ebp+24]
 section .text
 
 invertir_asm:
+	convencion_c_in 0
+	
+	mov esi, src
+	mov edi, dst
+	xor edx,edx
 
+cicloFila:
+	xor ecx,ecx
+	mov eax, ALTO
+	cmp edi, eax
+	
+	cicloColumna:
+		mov eax, RZ
+		
+
+
+
+	convencion_c_out 0
 	ret
--- tp2-entregable/src/invertir_c.c
+++ tp2-entregable/src/invertir_c.c
@@ -1,3 +1,9 @@
 void invertir_c (unsigned char *src, unsigned char *dst, int m, int n, int row_size) {
 
+   
+        for(int f = 0 ; f < m ; f++) {
+                for(int c = 0 ; c < n ; c++) {
+                        dst[ c + (row_size * f)] = 255 - src[c + (row_size * f)];
+                }
+        }
 }
