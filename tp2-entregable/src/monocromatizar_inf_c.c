c439635dd8ea030325450b35bffda4017af33d94
tolacelave <tolacelave@4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa>
2011-09-14 21:04:40 +0000

    monocromo C
    git-svn-id: https://022011-tp-o2.googlecode.com/svn/trunk@8 4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa

--- tp2-entregable/src/monocromatizar_inf_c.c
+++ tp2-entregable/src/monocromatizar_inf_c.c
@@ -1,5 +1,32 @@
 #define max(a, b) ((a)>(b))?(a):(b)
 
 void monocromatizar_inf_c(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size) {
+	int contadorFilas=0;
+	int contadorColumnas=0;
+	int desplazamiento=0; // me pasan una "martiz" en realidad es un vector muy grande de los cuales tengo que fijarme que hacer
+	int desplazamientoEnDestino=0;
 
+	while(contadorFilas<h){
+		contadorColumnas=0;
+		desplazamiento=0;
+		desplazamientoEnDestino=0;	
+		
+		while(contadorColumnas<w){//cada pixel esta compuesto por 3 bytes en la matriz de src
+			// estan en el orden B| G | R |
+			int blue = src[desplamiento+(contadorFilas*src_row_size)];  
+			int green = src[desplazamiento+(contadorFilas*src_row_size)+1];
+			int red= src[desplazamiento+(contadorFilas*src_row_size)+2];
+			
+			int maxTemporal = max(blue,green);
+			int resultado = max(red,maxTemporal);
+			
+			dst[desplazamientoEnDestino+(contadorFilas*dst_row_size)] = resultado;
+
+			desplazamiento = desplazamiento+3;
+			
+			desplazamientoEnDestino++;
+			contadorColumnas++;
+		}
+		contadorFilas++;
+	}
 }
--- tp2-entregable/src/monocromatizar_uno_c.c
+++ tp2-entregable/src/monocromatizar_uno_c.c
@@ -1,3 +1,32 @@
 void monocromatizar_uno_c(unsigned char *src, unsigned char *dst, int h, int w, int src_row_size, int dst_row_size) {
+	int contadorFilas=0;
+	int contadorColumnas=0;
+	int desplazamiento=0; // me pasan una "martiz" en realidad es un vector muy grande de los cuales tengo que fijarme que hacer
+	int desplazamientoEnDestino=0;
+	
+	//  obs usar i filas, i columnas inv: forall i, j src[i][j] == dst[i][j-3] AND  i <h, j<w AND
+//	blue == src[i][j] 	AND green == src[i][j+1] AND red == src[i][j+2] 
+	while(contadorFilas<h){
+		contadorColumnas=0;
+		desplazamiento=0;
+		desplazamientoEnDestino=0;	
+		
+		
+		while(contadorColumnas<w){//cada pixel esta compuesto por 3 bytes en la matriz de src
+			// estan en el orden B| G | R |
+			int blue = src[desplamiento+(contadorFilas*src_row_size)];  
+			int green = src[desplazamiento+(contadorFilas*src_row_size)+1];
+			int red= src[desplazamiento+(contadorFilas*src_row_size)+2];
+			
+			int resultado = (red+(2*green)+blue)/4;
+			
+			dst[desplazamientoEnDestino+(contadorFilas*dst_row_size)] = resultado;
 
+			desplazamiento = desplazamiento+3;
+			
+			desplazamientoEnDestino++;
+			contadorColumnas++;
+		}
+		contadorFilas++;
+	}
 }
