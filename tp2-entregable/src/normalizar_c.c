341bf16ee456bf452d82af45674134ab040182db
ferlores@gmail.com <ferlores@gmail.com@4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa>
2011-09-14 11:41:49 +0000

    primero implementacion en C de normalizar y separar_canales
    git-svn-id: https://022011-tp-o2.googlecode.com/svn/trunk@5 4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa

--- tp2-entregable/src/normalizar_c.c
+++ tp2-entregable/src/normalizar_c.c
@@ -1,3 +1,18 @@
 void normalizar_c (unsigned char *src, unsigned char *dst, int m, int n, int row_size) {
+    unsigned char max = 0;
+    unsigned char min = 255;
+    unsigned char *src2 = src;
+    
+    //busca max y min
+    for(unsigned int i=m*n; i > 0; i--){
+        if(*(src) > max) max = *(src);
+        if(*(src) < min) min = *(src);
+        src++;
+    }
 
+    //modifica src
+    for(unsigned int i=m*n; i > 0; i--){
+        *(dst++) = 255 * ( *(src2++) - min ) / ( max - min );
+    }
 }
+
--- tp2-entregable/src/separar_canales_c.c
+++ tp2-entregable/src/separar_canales_c.c
@@ -1,3 +1,9 @@
 void separar_canales_c (unsigned char *src, unsigned char *dst_r, unsigned char *dst_g, unsigned char *dst_b, int m, int n, int src_row_size, int dst_row_size) {
-
+    //recorre el src
+    for(unsigned int i=m*n; i > 0; i--){
+        *(dst_b++) = *(src++);
+        *(dst_g++) = *(src++);
+        *(dst_r++) = *(src++);
+    }
 }
+
