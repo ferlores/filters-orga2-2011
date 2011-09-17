5e4fe10a7f59be036fdffcc86964fd34e3c01576
ferlores@gmail.com <ferlores@gmail.com@4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa>
2011-09-17 16:12:23 +0000

    Ciclo base
    git-svn-id: https://022011-tp-o2.googlecode.com/svn/trunk@17 4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa

--- tp2-entregable/src/invertir_asm.asm
+++ tp2-entregable/src/invertir_asm.asm
@@ -5,30 +5,61 @@ global invertir_asm
 
 %include "macros.asm"
 
-%define src [ebp+8]
-%define dst [ebp+12]
-%define alto [ebp+16]
-%define ancho [ebp+20]
-%define RZ [ebp+24]
+section .data
+mascara_255: dd 0xffffffff,0xffffffff,0xffffffff,0xffffffff
+
+
+
 section .text
 
 invertir_asm:
 	convencion_c_in 0
-	
-  ; mov esi, src
-  ; mov edi, dst
-  ; xor edx,edx
+	mov esi, src		                            ; esi <- *src
+	mov edi, dst		                            ; edi <- dst
+	mov ecx, h                                      ; ecx <- h
+    ;----------------------------------
+    ; esi <- *src
+    ; edi <- dst
+    ; ecx <- h
+    ; ebx <- #columnas_procesada
+    ;----------------------------------
+cicloFila:                                          ; WHILE(h!=0) DO
+	xor ebx,ebx                                     ;     #columnas_p <- 0    
+	cicloColumna:	                                ;     WHILE(#columnas_p < row_size) DO
 
-;cicloFila:
-  ; xor ecx,ecx
- ;  mov eax, ALTO
-  ; cmp edi, eax
-	
-  ; cicloColumna:
-	 ;  mov eax, RZ
+;XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+;       INSERTE SU CODIGO PARA ITERAR AQUI!
+;XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+		movdqu xmm0, [esi+ebx]
+		;~ movdqu xmm3,[mascara_255]
+		;~ psubd xmm0,xmm3
+
+		movdqu [edi+ebx],xmm0
+
+;XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX				   
+		add ebx ,16                                 ;        #columnas_p <- #columnas_p + 16
+		mov eax, row_size                           ;        eax <- row_size
+		sub eax, ebx                                ;        eax <- row_size - #columnas_p
+        
+		cmp eax, 16                                 ;        IF (row_size - #columnas_p) < 16
+		jge cicloColumna                            ;          CONTINUE
 		
+        cmp eax,0                                   ;        IF (row_size - #columnas_p)
+		je termineCol                               ;          BREAK
+		
+        ;ultimos pixeles
+        mov ebx, row_size                           ;        ebx <- row_size
+        sub ebx,17                                  ;        ebx <- row_size - 17
+        jmp cicloColumna                            ;      ENDWHILE
+	
+    termineCol:
+		
+		add esi,row_size                            ;   src <- src + row_size
+		add edi,row_size                            ;   dst <- dst + row_size
+        dec ecx                                     ;   h--
+		jnz cicloFila                               ; ENDWHILE
 
-
-
+fin_invertir:
 	convencion_c_out 0
 	ret
+
