d6a03a824efd73355fd3ca85d0730ab1ae1c2152
tolacelave <tolacelave@4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa>
2011-09-17 02:50:43 +0000

    git-svn-id: https://022011-tp-o2.googlecode.com/svn/trunk@16 4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa

--- tp2-entregable/src/monocromatizar_uno_asm.asm
+++ tp2-entregable/src/monocromatizar_uno_asm.asm
@@ -25,33 +25,38 @@ monocromatizar_uno_asm:
 	mov esi, src ;
 	mov edi, dst ; 
 	
-	cicloFilas:
-			
-			mov edx, w
-			
-			clicloCols:
-				;mov eax,esi
-				;mov ecx, w
-				;sub ecx, edx
-
-				;lea eax, [eax+ecx] ; podria hacer un add pero "en teoria(segund david)" lea es mas rapido porque tiene una alu aparte
 				
-			
-				movdqu  xmm0, [esi] ; xmm0 = |B5|R4|G4|B4|R3|G3|B3|R2|G2|B2|R1|G1|B1|R0|G0|B0
-				movdqu xmm1, xmm0  ; xmm1 = |B5|R4|G4|B4|R3|G3|B3|R2|G2|B2|R1|G1|B1|R0|G0|B0
-				movdqu xmm2, xmm0  ; xmm2 = |B5|R4|G4|B4|R3|G3|B3|R2|G2|B2|R1|G1|B1|R0|G0|B0
+	movdqu  xmm0, [esi] ; xmm0 = B0|G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5
+	movdqu xmm1, xmm0  ; xmm1 =  B0|G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5
+	movdqu xmm2, xmm0  ; xmm2 =  B0|G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5
 				
 				
-				psrldq	xmm1, 1  ;xmm1 = 	| 0|B5|R4|G4|B4|R3|G3|B3|R2|G2|B2|R1|G1|B1|R0|G0|	; me corro 1 byte pero creo que es a izq
-				psrldq  xmm2, 2  ; xmm2 = 	| 0|0 |B5|R4|G4|B4|R3|G3|B3|R2|G2|B2|R1|G1|B1|R0| ; me corro 2
-								; xmm0 = 	|B5|R4|G4|B4|R3|G3|B3|R2|G2|B2|R1|G1|B1|R0|G0|B0 |
-				
-				
-				
+	pslldq	xmm1, 1  ;xmm1 =     G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5|0
+	pslldq  xmm2, 2  ; xmm2 = 	 R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5|0 |0
+					 ;xmm0 =     B0|G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5
+	
+	; multiplico por dos el xmm2			
+	psrlw xmm1,1      ; multiplico por dos cada word del xmm1 = G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5|0 *2 
+	paddb xmm0,xmm1 ;
+	paddb xmm0,xmm2 ;
+	
+	psllw xmm0 ,2 ; divido por 4 cada word => xmm0 = (B0+2*G0+R0)/4|  G0  |  R0|  (B1+2*G1+R1)/4| G1|R1|B2 +2*...  |G2|R2|B3+2* ..|G3|R3|B4+2* ..|G4|R4|B5
+	
+	;pand xmm0,  0xFF0000FF0000FF0000FF0000FF0000FF	
+	; ahora como los meto:		
+						;xmm0=cuentapix0  | 0 | 0| cuentapix1 |0 |0 | cuentapix2 | 0 |0 | cuenta pix3 | 0| 0| cuenta pix4 | 0 |0 | ??? 
+	
+	movdqu xmm2, xmm0  	;xmm2= cuentapix0 | 0 | 0| cuentapix1 |0 |0 | cuentapix2 | 0 |0 | cuenta pix3 | 0| 0| cuenta pix4 | 0 |0 | ??? 
+	movdqu xmm3, xmm0;  	;xmm3= cuentapix0 | 0 | 0| cuentapix1 |0 |0 | cuentapix2 | 0 |0 | cuenta pix3 | 0| 0| cuenta pix4 | 0 |0 | ??? 
+	
+	psrldq xmm2, 1     ;xmm2=           0 |cuentapix0 | 0 | 0| cuentapix1 |0 |0 | cuentapix2 | 0 |0 | cuenta pix3 | 0| 0| cuenta pix4 | 0 |0 |
+	psrldq xmm3, 2     ;xmm2=      0 | 0  |cuentapix0 | 0 | 0| cuentapix1 |0 |0 | cuentapix2 | 0 |0 | cuenta pix3 | 0| 0| cuenta pix4 | 0 |
 	
 	
 	
-	
-	
+	pop ebx
+	pop edi
+	pop esi
+	pop ebp
 	
 	ret
