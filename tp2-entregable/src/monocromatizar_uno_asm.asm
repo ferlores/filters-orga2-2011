00202966f59ca5738ee4685ea19b3314ef6341bc
tolacelave <tolacelave@4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa>
2011-09-21 02:16:52 +0000

    monocroatizar un
    git-svn-id: https://022011-tp-o2.googlecode.com/svn/trunk@39 4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa

--- tp2-entregable/src/monocromatizar_uno_asm.asm
+++ tp2-entregable/src/monocromatizar_uno_asm.asm
@@ -2,13 +2,24 @@
 
 global monocromatizar_uno_asm
 
+%define src_row_size [ebp+24]
+%define dst_row_size [ebp+28]
+
 %include "macros.asm"
+
 
 section .text
 
 
 monocromatizar_uno_asm:
 	convencion_c_in 0
+	
+pxor xmm7, xmm7									
+	mov ecx, 0xFFFFFFFF								
+	pinsrw xmm7, ecx, 0								
+	pinsrw xmm7, ecx, 3
+	pinsrw xmm7, ecx, 6								; xmm7 <- |FF|00|00|FF|00|00|FF|00
+
 	
 	mov esi, src		                            ; esi <- *src
 	mov edi, dst		                            ; edi <- dst
@@ -22,45 +33,83 @@ monocromatizar_uno_asm:
 
 cicloFila:                                          ; WHILE(h!=0) DO
 	xor ebx,ebx                                     ;     #columnas_p <- 0    
+	xor edx, edx                                    ;     #columnas_p_dst <- 0    
+	
 	cicloColumna:	                                ;     WHILE(#columnas_p < row_size) DO
 
 		
 		movdqu xmm0, [esi+ebx]; xmm0 = B0|G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5
+	
+		carga_distinto_ultima_columna:
+		
 		movdqu xmm1, xmm0  ; xmm1 =  B0|G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5
 		movdqu xmm2, xmm0  ; xmm2 =  B0|G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5
 				
 				
-		pslldq	xmm1, 1  ;xmm1 =     G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5|0
-		pslldq  xmm2, 2  ; xmm2 = 	 R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5|0 |0
+		psrldq	xmm1, 1  ;xmm1 =     G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5|0
+		psrldq  xmm2, 2  ; xmm2 = 	 R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5|0 |0
+					
 					 ;xmm0 =     B0|G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5
 	
 		; debo desempaquetar xmm1 shit!
 		pxor xmm3, xmm3    ; dos registro con cero	
 		movdqu xmm5, xmm1
 	
-		punpcklbw xmm1, xmm3   ; xmm1 <- G0|0|R0|0|B1|0|G1|0|R1|0|B2|0|G2|0|R2|0
-		punpckhbw xmm5, xmm3;    xmm5 <- B3|0|G3|0|R3|0|B4|0|G4|0|R4|0|B5|0|0|0	 
+		punpcklbw xmm1, xmm3   ; xmm1 <- G0|0|R0|0|B1|0|G1|0|R1|0|B2|0|G2|0|R2|0 xmm1 parte baja de operndo 2
+		
+		punpckhbw xmm5, xmm3;    xmm5 <- B3|0|G3|0|R3|0|B4|0|G4|0|R4|0|B5|0|0|0	 xmm5 parte alta de operando2
 	
-		psllw xmm1,1 ; xmm1 <- G0|0|R0|0|B1|0|G1|0|R1|0|B2|0|G2|0|R2|0 *2 de a words
-		psllw xmm5,1 ; xmm5 <- B3|0|G3|0|R3|0|B4|0|G4|0|R4|0|B5|0|0|0	*2 de a words 
+		psrlw xmm1,1 ; xmm1 <- G0|0|R0|0|B1|0|G1|0|R1|0|B2|0|G2|0|R2|0 *2 de a words
+		psrlw xmm5,1 ; xmm5 <- B3|0|G3|0|R3|0|B4|0|G4|0|R4|0|B5|0|0|0	*2 de a words 
 		; empaqueto de nuevo para tenerlos de byte
-		packuswb  xmm1, xmm5 ; xmm1 <- G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5|0 * 2
+		;packuswb  xmm1, xmm5 ; xmm1 <- G0|R0|B1|G1|R1|B2|G2|R2|B3|G3|R3|B4|G4|R4|B5|0 * 2
 	
-		paddb xmm0,xmm1 ;  llamo "s" a b+2*g+r
-		paddb xmm0,xmm2 ;  xmm0 <- s0|x|x|s1|x|x|s2|x|x|s3|x|x|s4|x|x|x 
-		; misma historia para multiplicar por 4
 		pxor xmm3,xmm3
-		movdqu xmm5, xmm0
-	
-		punpcklbw xmm0,xmm3 ; xmm0 <- s0|0|x|0|x|0|s1|0|x|0|x|0|s2|0|x|0
-		punpckhbw xmm5,xmm3  ;xmm5 <-  x|0|s3|0|x|0|x|0|s4|0|x|0|x|0|x|0 
-		psrlw xmm0,2	;    xmm0  <- s0|0|x|0|x|0|s1|0|x|0|x|0|s2|0|x|0    /4
-		psrlw xmm5,2    ;    xmm5  <-  x|0|s3|0|x|0|x|0|s4|0|x|0|x|0|x|0      /4
-	
-		;llamo  "c" a la cuenta rara que quria hacer
-		packuswb  xmm0,xmm5 ;    c0|x|x|c1|x|x|c2|x|x|c3|x|x|c4|x|x|x
-		; hasta aca tengo los resultados para empezar a hacer la logica del programa
-		; como voy a procesar con los 4 primero ( hasta c3 la logica es la misma que monocromINF)
+		movdqu xmm4, xmm0
+		punpcklbw xmm0, xmm3   ; xmm0 <- B0|0|G0|0|R0|0|B1|0|G1|0|R1|0|B2|0|G2|0      xmm0 parte baja de operando 1
+		punpckhbw xmm4, xmm3   ; xmm4 <- R2|0|B3|0|G3|0|R3|0|B4|0|G4|0|R4|0|B5|0     xmm4 pate alta de operando1
+		
+		;pxor xmm2,xmm2
+		pxor xmm6,xmm6
+		movdqu xmm3, xmm2
+		punpcklbw xmm2, xmm6 ;xmm2 <- R0|0|B1|0|G1|0|R1|0|B2|0|G2|0|R2|0|B3|0   xmm2 parte baja de operando 3
+		punpckhbw xmm3, xmm6 ;xmm3 <- G3|0|R3|0|B4|0|G4|0|R4|0|B5|0|0|0|0  |0   xmm3 parte alta de operando 3
+		
+		;sumemos partes bajas
+		paddb xmm0,xmm1
+		paddb xmm0,xmm2    ; xmm0 suma partes bajas
+		
+		paddb xmm4, xmm3
+		paddb xmm4, xmm5	;xmm4 suma partes altas
+		
+		psllw xmm0, 2 ;divido por 4
+		psllw xmm4, 2 
+		
+		;ahora debo empaquetar
+		
+		;pxor xmm3, xmm3
+		packuswb  xmm0, xmm4 
+		
+		
+		
+		;~ 
+		;~ paddb xmm0,xmm1 ;  llamo "s" a b+2*g+r
+		;~ paddb xmm0,xmm2 ;  xmm0 <- s0|x|x|s1|x|x|s2|x|x|s3|x|x|s4|x|x|x 
+		;~ ; misma historia para multiplicar por 4
+		;~ pxor xmm3,xmm3
+		;~ movdqu xmm5, xmm0
+	;~ 
+		;~ punpcklbw xmm0,xmm3 ; xmm0 <- s0|0|x|0|x|0|s1|0|x|0|x|0|s2|0|x|0
+		;~ punpckhbw xmm5,xmm3  ;xmm5 <-  x|0|s3|0|x|0|x|0|s4|0|x|0|x|0|x|0 
+		;~ psrlw xmm0,2	;    xmm0  <- s0|0|x|0|x|0|s1|0|x|0|x|0|s2|0|x|0    /4
+		;~ psrlw xmm5,2    ;    xmm5  <-  x|0|s3|0|x|0|x|0|s4|0|x|0|x|0|x|0      /4
+	;~ 
+		;~ ;llamo  "c" a la cuenta rara que quria hacer
+		;~ packuswb  xmm0,xmm5 ;    c0|x|x|c1|x|x|c2|x|x|c3|x|x|c4|x|x|x
+		;~ ; hasta aca tengo los resultados para empezar a hacer la logica del programa
+		;~ ; como voy a procesar con los 4 primero ( hasta c3 la logica es la misma que monocromINF)
+		;~ 
+		
 		;-----logica-------------------
 		movdqu xmm1, xmm0    ; copio el registro para trabajarlo mejor
 	
@@ -94,34 +143,46 @@ cicloFila:                                          ; WHILE(h!=0) DO
 		
 		packuswb xmm0,xmm3 ; xmm0 <-   c0 |c1|c2 |c3 |c4 |c5 |0 |0 |0 |0 |0 |0 |0 |0 |0 |0
 	
-		movdqu [edi+ebx],xmm0
 		
 		
-
-	
+		
+		movd eax, xmm0	
+		
+		mov [edi+edx], eax  ; copio de a 4
 ;XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX				   
-		add ebx ,16                                 ;        #columnas_p <- #columnas_p + 16
-		mov eax, row_size                           ;        eax <- row_size
-		sub eax, ebx                                ;        eax <- row_size - #columnas_p
+		add edx ,4                                  ;        #columnas_p_dst <- #columnas_p_dst + 4
+		add ebx ,12                                 ;        #columnas_p <- #columnas_p + 16
+
+		mov eax, w			                        ;        eax <- w
+		lea eax, [eax + 2*eax]						;        eax <- 3*w
+	
+		sub eax, ebx                                ;        eax <- 3*w - #columnas_p
         
-		cmp eax, 16                                 ;        IF (row_size - #columnas_p) < 16
+		cmp eax, 16                                 ;        IF (3*w - #columnas_p) < 16
 		jge cicloColumna                            ;          CONTINUE
 		
-        cmp eax,0                                   ;        IF (row_size - #columnas_p)
+        cmp eax, 4                                   ;        IF (3*w - #columnas_p)
 		je termineCol                               ;          BREAK
 		
         ;ultimos pixeles
-        mov ebx, row_size                           ;        ebx <- row_size
-        sub ebx,17                                  ;        ebx <- row_size - 17
-        jmp cicloColumna                            ;      ENDWHILE
+        add ebx, eax		                        ;        ebx <- 3*w
+        sub ebx,16                                  ;        ebx <- 3*w - 17
+         mov edx, w			                        ;        edx <- dst_row_size
+        sub edx,4                                   ;        edx <- dst_row_size - 5
+
+		movdqu xmm0, [esi+ebx]						;        xmm0 <- ultimos_16b(src) |RB|GR   |BG|RB|GR|BG|RB|GR|
+		psrldq xmm0, 4								
+		
+        jmp carga_distinto_ultima_columna           ;      ENDWHILE
 	
     termineCol:
 		
-		add esi,row_size                            ;   src <- src + row_size
-		add edi,row_size                            ;   dst <- dst + row_size
+		add esi,src_row_size                            ;   src <- src + row_size
+		add edi,dst_row_size                            ;   dst <- dst + row_size
         dec ecx                                     ;   h--
 		jnz cicloFila                               ; ENDWHILE
 
+		
 fin_invertir:
 	convencion_c_out 0
 	ret
