42d664e861a74f36e52b7d1c10572d2d19324387
tolacelave <tolacelave@4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa>
2011-09-21 22:24:50 +0000

    monocromatizar uno CASI, estoy ahi no debo alterar el ciclo nomas
    git-svn-id: https://022011-tp-o2.googlecode.com/svn/trunk@42 4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa

--- tp2-entregable/src/monocromatizar_uno_asm.asm
+++ tp2-entregable/src/monocromatizar_uno_asm.asm
@@ -56,7 +56,6 @@ cicloFila:                                          ; WHILE(h!=0) DO
 		psrldq  xmm2, 2  ; xmm2 <- 	  0 |0 |B5|R4|G4|B4|R3|G3|B3|R2|G2|B2|R1|G1|B1|R0|
 		
 	
-		
 		pxor xmm3, xmm3    
 		movdqu xmm5, xmm1  ;  xmm5  <-   0 |B5|R4|G4|B4|R3|G3|B3|R2|G2|B2|R1|G1|B1|R0|G0|
 	
@@ -68,102 +67,67 @@ cicloFila:                                          ; WHILE(h!=0) DO
 		
 		psllw xmm5,1  ;xmm5  <- 0|0|0|B5|0|R4|0|G4|0|B4|0|R3|0|G3|0|B3           *2 de a words
 		
-	
-		pxor xmm3,xmm3
+
 		movdqu xmm4, xmm0      
 		punpcklbw xmm0, xmm3   ; xmm0 <-  0|G2|0|B2|0|R1|0|G1|0|B1|0|R0|0|G0|0|B0   xmm0 parte baja de operando 1 
 		
 		punpckhbw xmm4, xmm3   ; xmm4 <- 0|B5|0|R4|0|G4|0|B4|0|R3|0|G3|0|B3|0|R2     xmm4 pate alta de operando1
 		
-		;pxor xmm2,xmm2  
-		pxor xmm6,xmm6
-		movdqu xmm3, xmm2  ;  xmm3 <- 0 |0|B5|R4|G4|B4|R3|G3|B3|R2|G2|B2|R1|G1|B1|R0|   
-		punpcklbw xmm2, xmm6 ;xmm2 <- 0|B3|0|R2|0|G2|0|B2|0|R1|0|G1|0|B1|0|R0|    xmm2 parte baja de operando 3
+		
+		movdqu xmm6, xmm2  ;  xmm6 <- 0 |0|B5|R4|G4|B4|R3|G3|B3|R2|G2|B2|R1|G1|B1|R0|   
+		
+		punpcklbw xmm2, xmm3 ;xmm2 <-  0|B3|0|R2|0|G2|0|B2|0|R1|0|G1|0|B1|0|R0|    xmm2 parte baja de operando 3
 		
 		
-		punpckhbw xmm3, xmm6 ; 0|0|0|0|0|B5|0|R4|0|G4|0|B4|0|R3|0|G3|  xmm3 parte alta de operando 3 
+		punpckhbw xmm6, xmm3 ; xmm6 <- 0|0|0|0|0|B5|0|R4|0|G4|0|B4|0|R3|0|G3|  xmm6 parte alta de operando 3 
 		
 		;sumemos partes bajas
-		paddb xmm0,xmm1  ;xmm0 <-  +  0|G2|0|B2|0|R1|0|G1|0|B1|0|R0|0|G0|0|B0
+		paddw xmm0,xmm1  ;xmm0 <-  +  0|G2|0|B2|0|R1|0|G1|0|B1|0|R0|0|G0|0|B0
 						;             0|R2|0|G2|0|B2|0|R1|0|G1|0|B1|0|R0|0|G0|      *2 de a words
 		
 		paddw xmm0,xmm2    ;		  0|B3|0|R2|0|G2|0|B2|0|R1|0|G1|0|B1|0|R0|
-		; xmm0 suma partes bajas
+		; xmm0 suma partes bajas  llamo "s" a la suma sin la division por 4
 		
-		paddw xmm4, xmm3
-		paddw xmm4, xmm5	;xmm4 suma partes altas
-		
-		psrlw xmm0, 2 ;divido por 4
-		psrlw xmm4, 2 
-		
-		;ahora debo empaquetar
-		
-		;pxor xmm3, xmm3
-		packuswb  xmm0, xmm4 
+							;xmm0 <- xx|s2|xx|xx|s1|xx|xx|s0|   de a words
 		
 		
+		paddw xmm4, xmm3    ;xmm4 suma partes altas
+		paddw xmm4, xmm5	;xmm4 <- xx|xx|xx|s4|xx|xx|s3|xx|   de a words 
+			
+			; divido por 4 casa suma y tengo la cuenta que queria llamo c a la cuenta que quiero
 		
-		;~ 
-		;~ paddb xmm0,xmm1 ;  llamo "s" a b+2*g+r
-		;~ paddb xmm0,xmm2 ;  xmm0 <- s0|x|x|s1|x|x|s2|x|x|s3|x|x|s4|x|x|x 
-		;~ ; misma historia para multiplicar por 4
-		;~ pxor xmm3,xmm3
-		;~ movdqu xmm5, xmm0
-	;~ 
-		;~ punpcklbw xmm0,xmm3 ; xmm0 <- s0|0|x|0|x|0|s1|0|x|0|x|0|s2|0|x|0
-		;~ punpckhbw xmm5,xmm3  ;xmm5 <-  x|0|s3|0|x|0|x|0|s4|0|x|0|x|0|x|0 
-		;~ psrlw xmm0,2	;    xmm0  <- s0|0|x|0|x|0|s1|0|x|0|x|0|s2|0|x|0    /4
-		;~ psrlw xmm5,2    ;    xmm5  <-  x|0|s3|0|x|0|x|0|s4|0|x|0|x|0|x|0      /4
-	;~ 
-		;~ ;llamo  "c" a la cuenta rara que quria hacer
-		;~ packuswb  xmm0,xmm5 ;    c0|x|x|c1|x|x|c2|x|x|c3|x|x|c4|x|x|x
-		;~ ; hasta aca tengo los resultados para empezar a hacer la logica del programa
-		;~ ; como voy a procesar con los 4 primero ( hasta c3 la logica es la misma que monocromINF)
-		;~ 
+		psrlw xmm0, 2    ;xmm0 <- xx|c2|xx|xx|c1|xx|xx|c0|   de a words
+		psrlw xmm4, 2    ;xmm4 <- xx|xx|xx|c4|xx|xx|c3|xx|   de a words 
 		
-		;-----logica-------------------
-		movdqu xmm1, xmm0    ; copio el registro para trabajarlo mejor
+		;and con la mascara
+		pand xmm0,xmm7   ;xmm0 <- 00|c2|00|00|c1|00|00|c0|   de a words
+		
+		pslldq xmm7, 2  ; xmm7 <-  FF|00|00|FF|00|00|FF|00
+		
+		pand xmm4, xmm7  ;xmm4 <-  xx|00|00|c4|00|00|c3|00|   de a words 
 	
-		psrlw xmm1,8   ; xmm1 <-  0 m0 |0 x| 0 x | 0 m2 | 0 x | 0 x | 0 m4 |0 x  shiftee cada word 8 bits logico a derecha
-	
-		movdqu xmm2, xmm1  ; xmm2 <-  0 m0 |0 x| 0 x | 0 m2 | 0 x | 0 x | 0 m3 |0 x , me guarde una copia de xmm1
-	
-		psllw xmm0, 8  ;  xmm0 <- x 0 |c1 0|x 0| x 0 | c3 0 | x 0 | x 0 |c5 0
+		psrldq xmm7, 2  ; xmm7 <-  00|FF|00|00|FF|00|00|FF| , shift a iz dos bytes reacomodo mask
 		
-		psrlw xmm0, 8  ;  xmm0 <- 0 x |0 c1|0 x| 0 x | 0 c3 | 0 x | 0 x |0 c5
-	
-		pand xmm1, xmm7 ; xmm1 <- 0 c0 |0 0| 0 0 | 0 c2 | 0 0 | 0 0 | 0 c4 |0 0
-	
-		psrldq xmm7, 2  ; xmm7 <-  00 |FF  |00 |00 |FF  |00 |00  |FF |
-	
-		pand xmm0, xmm7 ;  xmm0 <- 0 0|0 c1|0 0|0 0|0 c3|0 0|0 0 |0 c5
-	
-		pslldq xmm7, 2  ; xmm7 <- |FF  |00 |00 |FF  |00 |00  |FF |00  , shift a iz dos bytes reacomodo mask
-	
-		por xmm0,xmm1 ; xmm0 <-   0 c0|0 c1|0 0|0 c2|0 c3|0 0|0 c4 |0 c5
-	
-		pshufd xmm0, xmm0, 01101100b  ; xmm0 <-  0 c0|0 c1|0 c4|0 c5|0 c3|0 0|0 0 |0 c2
-	
-		pshufhw xmm0, xmm0, 01100011b ; xmm0 <-  0 c0|0 c1|0 mc|0 c5|0 c2|0 c3|0 0 |0 0
-	
-		pshufd xmm0,xmm0,  11011000b  ; xmm0 <-  0 c0|0 c1|0 c2|0 c3|0 c4|0 c5|0 0 |0 0
-	
-		psllw xmm0, 8   		 ; xmm0 <-   c0 0|c1 0|c2 0|c3 0|c4 0|c5 0|0 0 |0 0  corri 8 bits cada word
+		paddw xmm0, xmm4 ;xmm0 <- xx|c2|00|c4|c1|00|c3|c0|
 		
-		pxor xmm3, xmm3
+		pshuflw xmm0, xmm0,  10011100b   ;xmm0 <- xx|c2|00|c4|00|c3|c1|c0|
 		
-		packuswb xmm0,xmm3 ; xmm0 <-   c0 |c1|c2 |c3 |c4 |c5 |0 |0 |0 |0 |0 |0 |0 |0 |0 |0
+		pshufd xmm0,xmm0,  11011000b     ;xmm0 <- xx|c2|00|c3|00|c4|c1|c0|
+		
+		pshufhw xmm0, xmm0,  11010010b  ;xmm0 <- xx|00|c3|c2|00|c4|c1|c0|
+		
+		pshufd xmm0, xmm0, 11011000b     ;xmm0 <- xx|00|00|c4|c3|c2|c1|c0|
+		
+		movd eax, xmm0
+		
 	
-		
-		
-		
-		movd eax, xmm0	
-		
 		mov [edi+edx], eax  ; copio de a 4
+		;movqu [edi+edx], mm0
 ;XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX				   
-		add edx ,4                                  ;        #columnas_p_dst <- #columnas_p_dst + 4
-		add ebx ,12                                 ;        #columnas_p <- #columnas_p + 16
-
+		add edx ,4                                 ;        #columnas_p_dst <- #columnas_p_dst + 4
+	                              ;        #columnas_p <- #columnas_p + 16
+		add ebx ,12
+		
 		mov eax, w			                        ;        eax <- w
 		lea eax, [eax + 2*eax]						;        eax <- 3*w
 	
