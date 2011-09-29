c0c20e9c21748189d97e09c88cf6096810902efe
ferlores@gmail.com <ferlores@gmail.com@4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa>
2011-09-29 22:35:43 +0000

    correccion
    git-svn-id: https://022011-tp-o2.googlecode.com/svn/trunk@72 4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa

--- tp2-entregable/src/suavizar_c.c
+++ tp2-entregable/src/suavizar_c.c
@@ -2,13 +2,13 @@ void suavizar_c (unsigned char *src, unsigned char *dst, int m, int n, int row_s
 
     for(unsigned int i=1; i < m-1; i++){
         for(unsigned int j=1; j < n-1 ; j++){
-            float res=0;
+            unsigned int res=0;
             
-            res += (src[(i-1)*row_size+(j-1)] + 2*src[(i-1)*row_size+j] + src[(i-1)*row_size+(j+1)])/16;
-            res += (2*src[i*row_size+(j-1)] + 4*src[i*row_size+j] + 2*src[i*row_size+(j+1)])/16;
-            res += (src[(i+1)*row_size+(j-1)] + 2*src[(i+1)*row_size+j] + src[(i+1)*row_size+(j+1)])/16;
+            res += src[(i-1)*row_size+(j-1)] + 2 * src[(i-1)*row_size+j] + src[(i-1)*row_size+(j+1)];
+            res += 2 * src[i*row_size+(j-1)] + 4 * src[i*row_size+j] + 2 * src[i*row_size+(j+1)];
+            res += src[(i+1)*row_size+(j-1)] + 2 * src[(i+1)*row_size+j] + src[(i+1)*row_size+(j+1)];
             
-            dst[i*row_size+j] = (unsigned char)res;
+            dst[i*row_size+j] = (unsigned char) (res/16);
         }
     }
 }
