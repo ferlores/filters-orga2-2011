88dbda860fedd39b4138b2ab351abead4ecbaee5
ferlores@gmail.com <ferlores@gmail.com@4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa>
2011-09-30 21:21:28 +0000

    git-svn-id: https://022011-tp-o2.googlecode.com/svn/trunk@80 4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa

--- tp2-entregable/mediciones/medirTodo.sh
+++ tp2-entregable/mediciones/medirTodo.sh
@@ -4,5 +4,8 @@ rm mediciones.csv
 rm -rf ../data/test-in/*
 ../tests/testgen.sh
 
-./medir.sh casoByN.in
-./medir.sh casoColor.in
+./medir.sh casoBase
+./medir.sh casoByN-alto
+./medir.sh casoColor-alto
+./medir.sh casoAncho
+./medir.sh predictorSaltos
