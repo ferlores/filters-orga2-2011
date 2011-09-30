5326fbc3265fb8aba7442bed8b031cdaac051c9e
ferlores@gmail.com <ferlores@gmail.com@4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa>
2011-09-30 18:09:10 +0000

    Nice para las mediciones + caso de prueba del predictor de saltos
    git-svn-id: https://022011-tp-o2.googlecode.com/svn/trunk@74 4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa

--- tp2-entregable/mediciones/medir.sh
+++ tp2-entregable/mediciones/medir.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
-. ./$1 
+#. ./$1 
+
+IMGINFILE=$1/imagenes.in
+FUNCINFILE=$1/funciones.in
+
 
 IMGINDIR=../data/test-in
 MEDOUTFILE=./mediciones.csv
@@ -26,9 +30,9 @@ do
         img=`echo $file | cut -d'.' -f4`
         
         
-        c=`$BINFILE -t $ITER -i c $func $file 60 140| grep -e 'llamada' | cut -d':' -f 2  |  cut -d ' ' -f2`
+        c=`sudo nice -n 20 $BINFILE -t $ITER -i c $func $file 60 140| grep -e 'llamada' | cut -d':' -f 2  |  cut -d ' ' -f2`
     
-        asm=`$BINFILE -t $ITER -i asm $func $file 60 140| grep -e 'llamada' | cut -d':' -f 2  |  cut -d ' ' -f2`
+        asm=`sudo nice -20 $BINFILE -t $ITER -i asm $func $file 60 140| grep -e 'llamada' | cut -d':' -f 2  |  cut -d ' ' -f2`
     
         echo $img,$c,$asm 
         echo $img,$c,$asm  >> $MEDOUTFILE
