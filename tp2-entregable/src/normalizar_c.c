1464f785c0a1ffc9c9982099e53919936c63e15e
ferlores@gmail.com <ferlores@gmail.com@4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa>
2011-10-01 04:36:51 +0000

    git-svn-id: https://022011-tp-o2.googlecode.com/svn/trunk@83 4a1c01bc-288a-ff05-eb80-2dd4a42eb3fa

--- tp2-entregable/src/normalizar_c.c
+++ tp2-entregable/src/normalizar_c.c
@@ -1,7 +1,6 @@
 void normalizar_c (unsigned char *src, unsigned char *dst, int m, int n, int row_size) {
     unsigned char max = 0;
     unsigned char min = 255;
-    unsigned char *src2 = src;
     
     //busca max y min
     for(unsigned int i=0; i < m; i++){
